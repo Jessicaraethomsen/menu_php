500af7e5465df3cd07b11a9fa71c797fe466c6ce
Jessicaraethomsen <jessicathomsen@Jessicas-MacBook-Pro-2.local>
2016-09-23 08:58:06 +0200

    Adding Class=Active, Basename Function & Super Global Variable
    Adding the Class=“active”:  We use a class called active so that when
    we style the page, we can allow the user to know what page is they are
    actively on.  One can do this by adding an IF statement.
    
    IF statement:  Our IF statement here says if the variable $curpage is
    equal to for example the“about” then  the php should echo active.
    Declaring that page is active. The rest would be in CSS
    
    The basename() function: returns the filename from the path. While the
    Superglobal $_SERVER: holds information about headers, paths, and
    script locations. Then the PHP_SELF returns the filename of the
    currently executing script.
    
    By combining the $_SERVER & PHP_SELF it executes the script.

--- menu.php
+++ menu.php
@@ -1,7 +1,19 @@
+<?php 
+  $curpage = basename($_SERVER['PHP_SELF']);
+  
+  /*One can't use a class directly because they are on all pages, so it is better to use a conditional statement. IF statement*/  
+/* basename The basename() function returns the filename from a path.*/
+/*$_SERVER is a PHP super global variable which holds information about headers, paths, and script locations.*/
+/*PHP_SELF	Returns the filename of the currently executing script*/
+/* IF ==(equals) the php page then echo(show) the class called active */
+
+?>
+
 <ul>
-<il> <a href="index.php"> Home </a></il>
-<il> <a href="about.php">  About</a></il>
-<il> <a href="services.php"> Services </a></il>
-<il> <a href="blog.php"> Blog </a></il>
-<il> <a href="contact.php"> Contact </a></il>
+<il> <a href="index.php"<?php if($curpage == 'index.php') {echo 'class="active"'; }?>> Home </a></il>
+<il> <a href="about.php"<?php if($curpage == 'about.php') {echo 'class="active"'; }?>>  About</a></il>
+<il> <a href="services.php"<?php if($curpage == 'service.php') {echo '"class=active"'; }?>> Services </a></il>
+<il> <a href="blog.php"<?php if($curpage == 'blog.php') {echo 'class="active"'; }?>> Blog </a></il>
+<il> <a href="contact.php"<?php if($curpage == 'contact.php') {echo 'class="active"'; }?>> Contact </a></il>
 </ul>
+
